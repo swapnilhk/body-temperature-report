62de6566e3dc8180811eaeb065be086c85cc56f6
Swapnil Kharabe <swapnilhk@gmail.com>
2020-10-07 21:17:21 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles.css';
+import Navbar from "./Navbar"
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Navbar(){
+    return <header>Topcoder Body Temperature Report</header>
+}
+
+export default Navbar
